c393110653df9ff9bd3f635ac9d4e480da417382
Mehmet Ali <mehmetalid967@gmail.com>
2024-01-03 20:27:34 +0300

    l

--- Assets/CarSpawner.cs
+++ Assets/CarSpawner.cs
@@ -8,8 +8,6 @@ public class CarSpawner : MonoBehaviour
     public GameObject car;
     public Transform location;
     public Transform location2;
-    public float speed = 5;
-
 
     void Start()
     {
@@ -17,7 +15,7 @@ public class CarSpawner : MonoBehaviour
         StartCoroutine(carSpawner());
     }
 
-    IEnumerator carSpawner()
+   IEnumerator carSpawner()
     {
 
         while (true)
@@ -25,34 +23,10 @@ public class CarSpawner : MonoBehaviour
             yield return new WaitForSeconds(3);
             GameObject carObject = Instantiate(car, location);
 
-            //carObject.GetComponent<Rigidbody>().velocity = new Vector3(0.20f, 0.02f, 50f);
-            //MoveObject(carObject.GetComponent<Rigidbody>());
+            GameObject carObject2 = Instantiate(car, location2);
 
-
-
-            // GameObject carObject2 = Instantiate(car, location2);
-
-           // location.position = new Vector3(location.position.x, location.position.y, location.position.z + 100);
+            location.position = new Vector3(location.position.x, location.position.y, location.position.z + 30);
         }
-
+      
     }
-
-
-
-    void FixedUpdate()
-    {
-        // MoveObject();
-    }
-
-    void MoveObject(Rigidbody rb)
-    {
-        // Calculate the desired velocity based on the constant speed
-        Vector3 desiredVelocity = transform.forward * speed;
-
-        // Set the Rigidbody's velocity to the desired velocity
-        rb.velocity = desiredVelocity;
-    }
-
-
-
 }
