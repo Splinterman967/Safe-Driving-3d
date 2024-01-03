ae6c42bade138a8b0a3aff0f6f8ccdff7a1a878f
Mehmet Ali <mehmetalid967@gmail.com>
2024-01-03 20:27:34 +0300

    l

--- Assets/CarSpawner.cs
+++ Assets/CarSpawner.cs
@@ -5,13 +5,9 @@ using UnityEngine;
 public class CarSpawner : MonoBehaviour
 {
 
-    public GameObject[] Npcars;
+    public GameObject car;
     public Transform location;
     public Transform location2;
-    public Transform location3;
-    public Transform location4;
-    
-
 
     void Start()
     {
@@ -19,41 +15,18 @@ public class CarSpawner : MonoBehaviour
         StartCoroutine(carSpawner());
     }
 
-    IEnumerator carSpawner()
+   IEnumerator carSpawner()
     {
 
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(1, 3));
-            GameObject carObject =Instantiate(Npcars[Random.Range(0,4)],location);
-            yield return new WaitForSeconds(Random.Range(1, 3));
-            GameObject carObject2 = Instantiate(Npcars[Random.Range(0, 4)], location2);
-            yield return new WaitForSeconds(Random.Range(1, 3));
-            GameObject carObject3 = Instantiate(Npcars[Random.Range(0, 4)], location3);
-            yield return new WaitForSeconds(Random.Range(1, 3));
-            GameObject carObject4 = Instantiate(Npcars[Random.Range(0, 4)], location4);
+            yield return new WaitForSeconds(3);
+            GameObject carObject = Instantiate(car, location);
+
+            GameObject carObject2 = Instantiate(car, location2);
+
+            location.position = new Vector3(location.position.x, location.position.y, location.position.z + 30);
         }
-        
-    }
-
-    //void changeColor()
-    //{
-    //    GameObject carObject = Instantiate(car, location);
-    //    int i = 0;
-    //    Color random = new Color(Random.Range(0, 150), Random.Range(0, 150), Random.Range(0, 150));
-    //    while (i < 8)
-    //    {
-    //        Debug.Log(carObject.transform.GetChild(i).gameObject.GetComponent<Renderer>().materials[0]);
-    //        carObject.transform.GetChild(i).gameObject.GetComponent<Renderer>().materials[0].color=random;
-            
-    //        i++;
-            
-    //    }
-    //}
-
-
-    void FixedUpdate()
-    {
-       
+      
     }
 }
