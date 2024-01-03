36a3ee0f2922a0b825a092ece2cc56cb4ac9c8e1
Mehmet Ali <mehmetalid967@gmail.com>
2024-01-03 20:27:34 +0300

    l

--- Assets/CarSpawner.cs
+++ Assets/CarSpawner.cs
@@ -7,16 +7,26 @@ public class CarSpawner : MonoBehaviour
 
     public GameObject car;
     public Transform location;
+    public Transform location2;
 
     void Start()
     {
-        GameObject carObject = Instantiate(car, location);
 
+        StartCoroutine(carSpawner());
     }
 
-    // Update is called once per frame
-    void Update()
+   IEnumerator carSpawner()
     {
-        
+
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            GameObject carObject = Instantiate(car, location);
+
+            GameObject carObject2 = Instantiate(car, location2);
+
+            location.position = new Vector3(location.position.x, location.position.y, location.position.z + 30);
+        }
+      
     }
 }
